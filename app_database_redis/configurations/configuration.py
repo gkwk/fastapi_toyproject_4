c9b926795d38b5bf633e824413826e6a7b41712c
gkwk <124476558+gkwk@users.noreply.github.com>
2025-03-16 17:16:08 +0900

    Feat: app database redis 관련 기능 추가 Feat: app fastapi middleware Rate Limiter 추가 Fix: app fastapi lifespan 수정 Chore: mongodb_collection_name에서의 불필요한 코드 제거

---
+++ app_database_redis/configurations/configuration.py
@@ -0,0 +1,43 @@
+from functools import lru_cache
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import field_validator, Field
+
+from commons.enums import AppEnviromentType
+
+
+class Settings(BaseSettings):
+    APP_ENVIROMENT_TYPE: AppEnviromentType
+
+    FASTAPI_PORT: int
+
+    RDBMS_PORT: int
+    RDBMS_HOST_NAME: str
+    RDBMS_USERNAME: str
+    RDBMS_PASSWORD: str
+    RDBMS_DB_NAME: str
+
+    REDIS_PORT: int
+    REDIS_HOST_NAME: str
+
+    MONGODB_PORT: int
+    MONGODB_HOST_NAME: str
+    MONGODB_DB_NAME: str
+
+    RABBITMQ_PORT: int
+    RABBITMQ_USERNAME: str
+    RABBITMQ_PASSWORD: str
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore", use_enum_values=True)
+
+    def model_post_init(self, __context) -> None:
+        pass
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
+
+
+BASE_PATH: Path = Path().resolve()
---
+++ app_fastapi/core/middlewares/__init__.py
@@ -0,0 +1,1 @@
+from .rate_limiter_middleware import RateLimiterMiddleware
